9e5a51c493dd6f7f0dede4d216631e56e09459bb
Aleksandr Berezhnoy <alex@iMac-IMAC.local>
2025-06-19 02:55:22 +0300

    imports fixed

--- frontend_api/src/Pages/AuthPage.jsx
+++ frontend_api/src/Pages/AuthPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
-import AuthForm from "../components/auth/AuthForm";
+import AuthForm from "../components/Auth/AuthForm";
 import ToggleThemeButton from "../components/ToggleThemeButton";
 import styles from "./AuthPage.module.css";
 
